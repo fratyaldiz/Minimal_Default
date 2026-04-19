50bf1f23ea6630142a9235204504bc64c3e2407d
fratyaldiz <firatylz2004@hotmail.com>
2026-04-20 01:33:49 +0300

    Initial commit - Unreal project

---
+++ Source/Minimal_Default/Minimal_Default.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Minimal_Default.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Minimal_Default, "Minimal_Default" );
